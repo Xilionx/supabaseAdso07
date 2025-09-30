ec45284b8a291061f9445d65ceb5f7cb2fb4e041
dpinilla <ingdiegopinilla@hotmail.com>
2025-09-30 09:37:43 -0500

    Initial commit

---
+++ app/src/main/java/com/dap/supabase001/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.dap.supabase001
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dap.supabase001.ConexionService.ConexionServiceCajero
+import com.dap.supabase001.adapter.CajeroAdapter
+import com.dap.supabase001.databinding.ActivityMainBinding
+import com.dap.supabase001.model.CajeroViewModel
+import com.dap.supabase001.model.ModelCajero
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var cajeroAdapter: CajeroAdapter
+    lateinit var binding: ActivityMainBinding
+    private val model: CajeroViewModel by viewModels()
+    var dataset = mutableListOf<ModelCajero>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        recyclerView()
+        lifecycleScope.launch {
+            visualiza()
+        }
+        observer()
+    }
+
+    fun observer(){
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        model.currentName.observe(this, Observer { newName ->
+            // Update the UI, in this case, a TextView.
+            cajeroAdapter.dataSet = newName
+            cajeroAdapter.notifyDataSetChanged()
+        })
+    }
+
+    fun recyclerView(){
+        //dataset = arrayOf("January", "February", "March")
+        cajeroAdapter = CajeroAdapter(dataset)
+
+        //val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        binding.rvCajero.layoutManager = LinearLayoutManager(this)
+        binding.rvCajero.adapter = cajeroAdapter
+    }
+    suspend fun visualiza(){
+        withContext(Dispatchers.IO){
+            val result = ConexionServiceCajero.visualizaSupabase()
+            withContext(Dispatchers.Main){
+                model.addAllCajero(result)
+
+            }
+        }
+    }
+}
